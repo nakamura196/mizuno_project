8740f672764f85246b0195ad02ed53f3da2b6c14
ymaquarium <ymaquarium@gmail.com>
2019-11-14 13:38:36 +0900

    create urlgenerator for Curation Viewing

---
+++ utils/urlgenerator.py
@@ -0,0 +1,9 @@
+import sys
+import webbrowser
+
+original_url = sys.argv[1]
+curation     = original_url.lstrip("https://github.com/nakamura196/mizuno_project/blob/master/docs/curation/")
+curaView_url = "http://codh.rois.ac.jp/software/iiif-curation-viewer/demo/?curation=https://raw.githubusercontent.com/nakamura196/mizuno_project/master/docs/curation/{}&mode=annotation&lang=ja".format(curation)
+
+browser = "/Applications/Google\ Chrome.app"
+browser.open_new(curaView_url)
