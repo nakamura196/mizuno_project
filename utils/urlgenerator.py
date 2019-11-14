46e05764eb7c2c19085f76655fbce02465e9fedc
ymaquarium <ymaquarium@gmail.com>
2019-11-14 13:41:40 +0900

    Bug fixed

--- utils/urlgenerator.py
+++ utils/urlgenerator.py
@@ -5,5 +5,5 @@ original_url = sys.argv[1]
 curation     = original_url.lstrip("https://github.com/nakamura196/mizuno_project/blob/master/docs/curation/")
 curaView_url = "http://codh.rois.ac.jp/software/iiif-curation-viewer/demo/?curation=https://raw.githubusercontent.com/nakamura196/mizuno_project/master/docs/curation/{}&mode=annotation&lang=ja".format(curation)
 
-browser = "/Applications/Google\ Chrome.app"
+browser = webbrowser.get("open -a /Applications/Google\ Chrome.app %s")
 browser.open_new(curaView_url)
