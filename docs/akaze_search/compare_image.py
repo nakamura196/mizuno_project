415e4e3b80bc9d6673695dcdf3bc87eb154d2015
ymaquarium <ymaquarium@gmail.com>
2019-12-14 17:53:51 +0900

    implemented reading feature vectors from csv

--- docs/akaze_search/compare_image.py
+++ docs/akaze_search/compare_image.py
@@ -4,48 +4,32 @@ import numpy as np
 import pandas as pd
 import time
 
+#csvから特徴量を読み込む
 def read_des(csv_path):
-    des = pd.read_csv(csv_path).values
+    try:
+        des_int64 = pd.read_csv(csv_path).values
+    #空ファイルならスキップ用の目印出力
+    except pd.io.common.EmptyDataError:
+        return np.array(0)
+    des = des_int64.astype(np.uint8)
 
     return des
 
-def compare_image(query_name, hojo_name):
+def compare_image(query_name):
+    #検索画像の読み込み
     img = cv2.imread("./query/{}.jpg".format(query_name))
     detector = cv2.AKAZE_create()
 
     kp_q, des_q = detector.detectAndCompute(img, None)
 
-    images = os.listdir("./{}".format(hojo_name))
-    if ".DS_Store" in images:
-        images.remove(".DS_Store")
-
-
-    ranking = []
-    search_start = time.time()
-    for image in images:
-        img_t = cv2.imread("./{}/{}".format(hojo_name, image))
-        kp, des = detector.detectAndCompute(img_t, None)
-
-        bf = cv2.BFMatcher()
-        matches = bf.knnMatch(des, des_q, k=2)
-
-        point = 0
-        match_param = 0.7
-        for m, n in matches:
-            if m.distance < match_param*n.distance:
-                point += 1
-        ranking.append([point, image])
-    search_end = time.time()
-
-    """
-    print(len(des_q[0]))
-    print(type(des_q))
-
     search_targets = os.listdir("./feature_vecs_csv")
     if ".DS_Store" in search_targets:
         search_targets.remove(".DS_Store")
 
+    #検索開始
     ranking = []
+    search_start = time.time()
+    #feature_vecs_csv/にある各法帖の各ページ各行の特徴量と比較
     for target in search_targets:
         csv_names = os.listdir("./feature_vecs_csv/{}".format(target))
         if ".DS_Store" in csv_names:
@@ -53,8 +37,8 @@ def compare_image(query_name, hojo_name):
 
         for csv in csv_names:
             des = read_des("./feature_vecs_csv/{}/{}".format(target, csv))
-            print(len(des[0]))
-            print(type(des))
+            if np.sum(des) == 0:
+                continue
 
             bf = cv2.BFMatcher()
             matches = bf.knnMatch(des, des_q, k=2)
@@ -66,13 +50,14 @@ def compare_image(query_name, hojo_name):
                     point += 1
             ranking.append([point, csv])
 
-    """
+    search_end = time.time()
     ranking.sort()
     ranking.reverse()
     for i in range(5):
         print(ranking[i])
 
     print(search_end-search_start)
+
 
 if __name__ == "__main__":
     compare_image("hojo4", "偽絳帖 三 [A005936-03]")
