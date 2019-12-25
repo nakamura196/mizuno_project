f9c9d98608b805b2e3ca285eeefa9bc26d63b5ac
ymaquarium <ymaquarium@gmail.com>
2019-12-25 12:36:32 +0900

    Bug fixed

--- hojo_detection_system/app.py
+++ hojo_detection_system/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from modules import compare, curation, download, preprocess
-#from flask_cors import CORS
+from flask_cors import CORS
 
 #インスタンス化
 app = Flask(__name__)
 
-#CORS(app)
+CORS(app)
 
 app.config['JSON_AS_ASCII'] = False  # 日本語文字化け対策
 app.config["JSON_SORT_KEYS"] = False  # ソートをそのまま
@@ -19,10 +19,10 @@ def hello():
 def curator():
     default_url = "https://iiif.dl.itc.u-tokyo.ac.jp/repo/iiif-img/167904/4872,2848,568,1344/full/0/default.jpg"
     im_url = request.args.get("url", default=default_url, type=str)
-    im_url_resized = im_url.replace("full", "150,") #リサイズはあとで検討する、URLに埋め込むより普通にあとでリサイズした方がいい？
+    #im_url_resized = im_url.replace("full", "150,") #リサイズはあとで検討する、URLに埋め込むより普通にあとでリサイズした方がいい？
 
     #クエリ画像をダウンロード
-    download.download_image(im_url_resized)
+    download.download_image(im_url)#_resized)
 
     #クエリ画像を加工
     preprocess.resize()
@@ -36,6 +36,8 @@ def curator():
     curationlist = curation.generate_curationList(ranking_top5_hojo)
 
     return jsonify(curationlist)
+
+    #myjsonにPOSTして帰ってきたURLをGET/POSTで受け取って、それをreturn
 
 if __name__ == '__main__':
     app.debug = True # デバッグモード有効化
