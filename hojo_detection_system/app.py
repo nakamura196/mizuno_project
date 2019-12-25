7202904a0ee35e4115e0fe97f13a8e882a798cdb
ymaquarium <ymaquarium@gmail.com>
2019-12-25 12:06:52 +0900

    implemented GET url

--- hojo_detection_system/app.py
+++ hojo_detection_system/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from modules import compare, curation, download, preprocess
-from flask_cors import CORS
+#from flask_cors import CORS
 
 #インスタンス化
 app = Flask(__name__)
 
-CORS(app)
+#CORS(app)
 
 app.config['JSON_AS_ASCII'] = False  # 日本語文字化け対策
 app.config["JSON_SORT_KEYS"] = False  # ソートをそのまま
@@ -15,13 +15,14 @@ app.config["JSON_SORT_KEYS"] = False  # ソートをそのまま
 def hello():
     return "hello"
 
-@app.route('/hojodetection')
+@app.route('/hojodetection', methods=["GET", "POST"])
 def curator():
-    im_url = "https://iiif.dl.itc.u-tokyo.ac.jp/repo/iiif-img/167904/4872,2848,568,1344/full/0/default.jpg"
-    #im_url_resized = im_url.replace("full", "150,") #リサイズはあとで検討する、URLに埋め込むより普通にあとでリサイズした方がいい？
+    default_url = "https://iiif.dl.itc.u-tokyo.ac.jp/repo/iiif-img/167904/4872,2848,568,1344/full/0/default.jpg"
+    im_url = request.args.get("url", default=default_url, type=str)
+    im_url_resized = im_url.replace("full", "150,") #リサイズはあとで検討する、URLに埋め込むより普通にあとでリサイズした方がいい？
 
     #クエリ画像をダウンロード
-    download.download_image(im_url)
+    download.download_image(im_url_resized)
 
     #クエリ画像を加工
     preprocess.resize()
