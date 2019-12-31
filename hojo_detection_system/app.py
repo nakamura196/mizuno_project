52ba7f04415869fe3f48a9a59049a8ee048f0682
ymaquarium <ymaquarium@gmail.com>
2019-12-31 14:34:37 +0900

    check behavior

--- hojo_detection_system/app.py
+++ hojo_detection_system/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, request, jsonify
 from modules import compare, curation, download, preprocess
 from flask_cors import CORS
+import os, json, hashlib
 
 #インスタンス化
 app = Flask(__name__)
@@ -19,6 +20,17 @@ def hello():
 def curator():
     default_url = "https://iiif.dl.itc.u-tokyo.ac.jp/repo/iiif-img/167904/4872,2848,568,1344/full/0/default.jpg"
     im_url = request.args.get("url", default=default_url, type=str)
+
+    #既存の検索結果ならそのキュレーションを返す
+    calculated_results = os.listdir("./results/")
+    hashed_im_url = hashlib.sha1(im_url.encode()).hexdigest() + ".json"
+    if hashed_im_url in calculated_results:
+        with open("./results/{}".format(hashed_im_url), "r") as f:
+            output = json.load(f)
+
+        return jsonify(output)
+
+    #新規検索なら以降の処理をやる
     im_url_resized = im_url.replace("full", "150,") #幅150より小さいクエリ画像についてはあとで検討する
 
     #クエリ画像をダウンロード
@@ -35,9 +47,13 @@ def curator():
     #キュレーションリストを作る
     curationlist = curation.generate_curationList(ranking_top5_hojo)
 
-    return jsonify(curationlist)
+    #既存の検索結果として保存しとく
+    store_name = hashlib.sha1(im_url.encode()).hexdigest()
+    curationlist_js = json.dumps(curationlist)
+    with open("./results/{}.json".format(store_name), "w") as f:
+        f.write(curationlist_js)
 
-    #myjsonにPOSTして帰ってきたURLをGET/POSTで受け取って、それをreturn
+    return jsonify(curationlist)
 
 if __name__ == "__main__":
     app.debug = True
